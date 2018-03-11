f37c304d81c1d5c4f752f821ac117096acc392ac
Smell Zero <smellzero@Smellde-MacBook-Pro.local>
2018-03-11 15:31:54 +0800

    测试离线打包命令

--- ios/ZeroCityShop/AppDelegate.m
+++ ios/ZeroCityShop/AppDelegate.m
@@ -19,7 +19,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+ jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+ // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"react" fallbackResource:@"bundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ZeroCityShop"
